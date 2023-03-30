ae5f2fadb8ec5afe80ac6eac28e3ccfeb7e4ce20
Seseok97 <112687040+Seseok97@users.noreply.github.com>
2023-03-30 17:55:01 +0900

    0330, JAVA DB연결 소스

--- BoardDAO.java
+++ BoardDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.naming.Context;
@@ -56,6 +57,19 @@ public class BoardDAO {
 		return con;
 	}//getCon() method end
 	
+	// DB 자원해제 메서드
+	public void closeDB() {
+		try {
+			if(rs != null)		rs.close();
+			if(pstmt != null)   pstmt.close();
+			if(con != null) 	con.close();
+			// finally절에 실행될것이기때문에, if문을 통한 제어가 필요하다 !
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}// try-catch end
+		
+	}// closeDB method() end
+	
 	public void insertBoard(BoardDTO dto) throws Exception {
 		// 글쓰기 동작 수행
 		con = getCon();
@@ -107,6 +121,9 @@ public class BoardDAO {
 		pstmt.executeUpdate(); // Insert
 		
 		System.out.println("글쓰기 완료 !");
+		
+		closeDB();
+
 	}// insertBoard() method end
 	
 	/**
@@ -163,11 +180,13 @@ public class BoardDAO {
 		}// while
 		System.out.println("DAO: 게시판 글 정보 모두 저장 완료!");
 		
+		closeDB();
+		
 		return boardList;
 	}//getBoardListAll() method end
 	
 	// 커넥션풀 사용
-	public ArrayList getBoardList(int startRow, int pageSize) throws Exception {
+	public ArrayList getBoardList(int startRow, int pageSize)  {
 			
 			// ArrayList 만들기
 			ArrayList boardList = new ArrayList();
@@ -175,46 +194,62 @@ public class BoardDAO {
 			// 정보 가져오기 동작 수행
 			
 			// 1,2 디비연결 (커넥션풀)
-			con = getCon();
-			
-			String sql = "select * from itwill_board"
-					+ " limit ?,?";
-//					+ " where bno > 0"
-//					+ " order by bno desc";
-			
-			PreparedStatement pstmt = con.prepareStatement(sql);
-			
-			pstmt.setInt(1, startRow -1); // 시작 위치
-			pstmt.setInt(2, pageSize); // 글의 개수
-			
-			ResultSet rs = pstmt.executeQuery();
-			
-			// 데이터를 DB >> DTO로 옮기기 !
-			// DTO에 여러개의 데이터가 저장될 것이기 때문에 !! >> ArrayList 사용.
-			// 1 row > 1 DTO > 1 ArrayList
-			
-			
-			while(rs.next()) { // 데이터가 있을때 (=EOF에 도달하지 않는다면,)
-				// rs(1 row) > dto (1 dto)
-				BoardDTO dto = new BoardDTO();
-				dto.setBno(rs.getInt("bno"));
-				dto.setContent(rs.getString("content"));
-				dto.setDate(rs.getDate("date"));
-				dto.setFile(rs.getString("File"));
-				dto.setIp(rs.getString("ip"));
-				dto.setName(rs.getString("name")); // 6
-				dto.setPass(rs.getString("pass"));
-				dto.setRe_lev(rs.getInt("re_lev"));
-				dto.setRe_ref(rs.getInt("re_ref"));
-				dto.setRe_seq(rs.getInt("re_seq"));
-				dto.setReadcount(rs.getInt("readcount"));
-				dto.setSubject(rs.getString("subject"));
+			try {
+				con = getCon();
+				String sql = "select * from itwill_board"
+						+ " order by re_ref desc, re_seq asc limit ?,?";
+//						+ " where bno > 0"
+//						+ " order by bno desc";
 				
-				// 1 dto > 1 ArrayList
-				boardList.add(dto);
+				pstmt = con.prepareStatement(sql);
 				
-			}// while
-			System.out.println("DAO: 게시판 글 정보 모두 저장 완료!");
+				pstmt.setInt(1, startRow -1); // 시작 위치
+				pstmt.setInt(2, pageSize); // 글의 개수
+				
+				rs = pstmt.executeQuery();
+				
+				// 데이터를 DB >> DTO로 옮기기 !
+				// DTO에 여러개의 데이터가 저장될 것이기 때문에 !! >> ArrayList 사용.
+				// 1 row > 1 DTO > 1 ArrayList
+				
+				
+				while(rs.next()) { // 데이터가 있을때 (=EOF에 도달하지 않는다면,)
+					// rs(1 row) > dto (1 dto)
+					BoardDTO dto = new BoardDTO();
+					dto.setBno(rs.getInt("bno"));
+					dto.setContent(rs.getString("content"));
+					dto.setDate(rs.getDate("date"));
+					dto.setFile(rs.getString("File"));
+					dto.setIp(rs.getString("ip"));
+					dto.setName(rs.getString("name")); // 6
+					dto.setPass(rs.getString("pass"));
+					dto.setRe_lev(rs.getInt("re_lev"));
+					dto.setRe_ref(rs.getInt("re_ref"));
+					dto.setRe_seq(rs.getInt("re_seq"));
+					dto.setReadcount(rs.getInt("readcount"));
+					dto.setSubject(rs.getString("subject"));
+					
+					// 1 dto > 1 ArrayList
+					boardList.add(dto);
+					
+				}// while
+				System.out.println("DAO: 게시판 글 정보 모두 저장 완료!");
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				// 예외여부와 상관없이 실행되는 구문 작성
+				// >> 자원 해제 (DB 연결 정보 해제 - 사용한 객체의 역순 종료)
+//				try {
+//					rs.close();
+//					pstmt.close();
+//					con.close();
+//				} catch (SQLException e) {
+//					e.printStackTrace();
+//				}
+				closeDB();
+				
+			}// finally end
 			
 			return boardList;
 		}//getBoardList() method end
@@ -234,15 +269,92 @@ public class BoardDAO {
 				result = rs.getInt(1);
 			}
 			System.out.println("DAO: 전체 글 개수 = "+ result);
+			pstmt.close();
 			con.close();
 			rs.close();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}finally {
+			closeDB();
+		} // finally end
 
 		return result;
 	}//getBoardCount() method end
+	
+	//getBoard()
+	public BoardDTO getBoard(int bno) {
+		BoardDTO dto=null; // 로컬변수로 선언되지 않도록 주의.
+						   // 왜 인스턴스 생성을 하지 않고 변수 저장만 해두나 ?
+						    //  >> 메모리 영역을 아끼기 위해서. > rs.next 가 true일때(데이터가 존재할때)만 !! 가져오면 메모리를 아낄수 있다 !!
+							//  >>> 당장에는 차이가 거의 나지 않지만, 이런것들이 조금조금 쌓이면서 커진다 !! (메모리 누수)
+		// 1,2 DB 연결
+		try {
+			con=getCon();
+			// 3 sql query & pstmt
+			sql="select * from itwill_board where bno =?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, bno);
+			// 4 sql execute
+			rs = pstmt.executeQuery();
+			
+			// 5 DATA 처리
+			if(rs.next()) {
+				//데이터 저장(rs >> dto)
+				dto = new BoardDTO();
+				dto.setBno(rs.getInt("bno"));
+				dto.setContent(rs.getString("content"));
+				dto.setDate(rs.getDate("date"));
+				dto.setFile(rs.getString("file"));
+				dto.setIp(rs.getString("ip"));
+				dto.setName(rs.getString("name"));
+				dto.setPass(rs.getString("pass"));
+				dto.setRe_lev(rs.getInt("re_lev"));
+				dto.setRe_ref(rs.getInt("re_ref"));
+				dto.setRe_seq(rs.getInt("re_seq"));
+				dto.setReadcount(rs.getInt("readcount"));
+				dto.setSubject(rs.getString("subject"));
+				//시간줄이는방법
+				// 1. 인덱스넘버로 들고오기
+				// 2. 지난번에 사용한 소스 복붙하기.
+				System.out.println("DAO: "+bno+"번 글, 정보 저장 완료!");
+			}// if end
+		} catch (Exception e) {
+			System.out.println("DAO: getBoard(int bno) Method Error!!");
+			e.printStackTrace();
+		}finally {
+			closeDB();
+		}// t-c-f end
+		// >> 위 내용이 항상 DB와 관련된 작업의 시작임 !!(t-c-f)
+		return dto;
+	}//getBoard() method end
+	
+	// updateReadCount() method
+	public void updateReadCount(int bno) {
+		try {
+			con=getCon();
+			// 특정 글의 조회수를 1 증가시키자.
+			sql = "update itwill_board set readcount=readcount+1"
+					+ " where bno=?";
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, bno);
+			
+			int cnt = pstmt.executeUpdate();
+			if(cnt == 1) {
+				// pstmt.executeUpdate() 메서드의 리턴값은 몇 줄이 영향을 받았는지를 int로 리턴한다 !!!
+				// cnt가 1이 아니라는것은 0이거나 1 초과 >> 정상적인 작동이 아님!!
+				System.out.println("DAO: updateReadCount() 정상작동, 글 조회수 1 증가.");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("DAO: updateReadCount() ERROR!!");
+		} finally {
+			closeDB();
+		}// t-c-f end
+		
+	}//updateReadCount() method end
 
 }// DAO class end
 
